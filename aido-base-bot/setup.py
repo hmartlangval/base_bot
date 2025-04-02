45e673af15c0432c8dfec6dc64bdf923b95303f7
nelvin-hmar <109198308+nelvin-tf@users.noreply.github.com>
2025-04-02 13:36:07 +0530

    v3.3.0 supports cancellation

--- aido-base-bot/setup.py
+++ aido-base-bot/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='base_bot',
-    version='3.2.1',
+    version='3.3.0',
     packages=find_packages(),
     install_requires=[
         'python-dotenv>=1.0.0',
